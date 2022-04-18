7e1be15ec6c36a651ad5defb5e4d00851ae03061
George Magdy <georgtriple@gmail.com>
2022-04-18 19:43:26 +0200

    Add relation between user and entity and update database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_18_173446) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_18_174155) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_18_173446) do
     t.float "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_entities_on_user_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -36,5 +38,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_18_173446) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "entities", "users"
   add_foreign_key "groups", "users"
 end
