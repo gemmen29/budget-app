220f044c77f4cc2726de127f78a9e8223275ab56
George Magdy <georgtriple@gmail.com>
2022-04-20 22:59:53 +0200

    Update redirect after delete transaction

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -60,7 +60,7 @@ class EntitiesController < ApplicationController
     @entity.destroy
 
     respond_to do |format|
-      format.html { redirect_to entities_url, notice: "Transaction was successfully destroyed." }
+      format.html { redirect_to groups_url, notice: "Transaction was successfully destroyed." }
       format.json { head :no_content }
     end
   end
